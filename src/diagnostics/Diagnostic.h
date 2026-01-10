5815875799cfcfb77d98b7bdca52306009ae21f4
Nagula Anish <anish.nagula@gmail.com>
2026-01-10 15:27:01 +0530

    added diagnostic data structure

---
+++ src/diagnostics/Diagnostic.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <string>
+
+enum class Severity {
+    Error,
+    Warning,
+    Note,
+    Unknown
+};
+
+struct Diagnostic {
+    std::string file;       // source file path
+    int line = -1;          // line number
+    int column = -1;        // column number
+
+    Severity severity = Severity::Unknown;
+    std::string message;    // compiler message
+
+
+    std::string normalized_id;
+}
